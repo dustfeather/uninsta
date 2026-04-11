4f453764b597c924a7a566ab7d4a7fcebc3d8bd7
Catalin Teodorescu <dustfeather@gmail.com>
2026-04-11 03:40:40 +0300

    fix: suppress error when content script is not loaded

--- src/background.ts
+++ src/background.ts
@@ -1,5 +1,7 @@
 chrome.action.onClicked.addListener((tab) => {
   if (tab.id) {
-    chrome.tabs.sendMessage(tab.id, { type: 'uninsta-toggle' });
+    chrome.tabs.sendMessage(tab.id, { type: 'uninsta-toggle' }).catch(() => {
+      // Content script not loaded on this page -- ignore
+    });
   }
 });
